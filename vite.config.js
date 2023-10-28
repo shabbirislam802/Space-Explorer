6532e0b602fa93e0494f2ddb59dc3b8f8d8bcaf7
shabbirislam <shabbirislam801@gmail.com>
2023-10-28 21:23:04 +0200

    - add base in vite file for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/Space-Explorer/',
     build: {
         rollupOptions: {
             input: {
