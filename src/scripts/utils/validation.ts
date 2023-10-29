01ea086cb307793f3d7ce71e8dd59f48968b4d8b
shabbirislam <shabbirislam801@gmail.com>
2023-10-29 14:36:58 +0100

    - add the removed export in validateEmail function

--- src/scripts/utils/validation.ts
+++ src/scripts/utils/validation.ts
@@ -34,7 +34,7 @@ export function validateForm(form: HTMLFormElement): boolean {
     return isValid;
 }
 
-function validateEmail(email: string): boolean {
+export function validateEmail(email: string): boolean {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email);
 }
